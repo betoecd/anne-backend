4e20e537a0ce9afee15649b20442f4f0d83e3979
Eberton Dias <eberton.bits@gmail.com>
2020-12-27 22:58:03 -0300

    🚧 New user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from djongo import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model.
+    Set username field to 'email'.
+    Set required fields 'first_name'.
+    Overwrite objects with our custom user manager.
+    """
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
+    occupation = models.CharField(max_length=100, blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    profile_title = models.CharField(
+        max_length=100,
+        blank=True,
+        default='What I\'m doing')
+    profile_description = models.TextField(blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
